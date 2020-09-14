739846d5bce0d73dcda4d021d281152123a63eec
Yesnor <yesnorayr@gmail.com>
2020-09-14 18:39:02 +0300

    1232

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className={"home__title u--fadeInUp"}>Micro Craft</h1>
       <h4 className={"home__desc u--fadeInUp"}>
         Custom and neat webdesign & front‑end development focused on
-        user‑friendly experiences.
+        user‑friendly experiences
       </h4>
       <button href="#" className={"home__btn u--fadeInUp"}>
         CHECK OUR WORK
